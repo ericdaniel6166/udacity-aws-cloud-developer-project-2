25886c08eeaa9e76caeefb45f18565a1b1514f7e
eric6166 <ericnguyen6166@gmail.com>
2022-04-21 01:05:13 +0700

    development

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+import prefix from "loglevel-plugin-prefix";
+
+let option = {
+    defaults: {
+        template: '[%t] %l:',
+        levelFormatter: function (level: any) {
+            return level.toUpperCase();
+        },
+        nameFormatter: function (name: any) {
+            return name || 'root';
+        },
+        timestampFormatter: function (date: any) {
+            return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+        }
+    }
+}
+
+export const log = require('loglevel');
+
+log.enableAll();
+prefix.reg(log);
+prefix.apply(log, option.defaults);
+
